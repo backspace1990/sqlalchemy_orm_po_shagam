c91e48b29423fe4ef18e3149c4f2c4e047f4ddac
Ümit Aşan <tgrandis@unknownea07de213aa2.home>
2024-11-12 01:28:44 +0300

    Hepsi son halini aldi

--- src/database.py
+++ src/database.py
@@ -1,31 +1,26 @@
 import asyncio
-from sqlalchemy import String, create_engine, text
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
-from config import settings
 from typing import Annotated
 
+from sqlalchemy import String, create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
+from src.config import settings
 
 sync_engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
     echo=True,
-    #pool_size=5,    #baglanti sayisi
-    #max_overflow=10  #yukaridaki baglanti sayisi dolunca ekstra 10 baglanti daha saglar
+    # pool_size=5,
+    # max_overflow=10,
 )
 
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
     echo=True,
-    
 )
 
-
-
-session_faktory = sessionmaker(sync_engine)
-asyn_session_faktory = async_sessionmaker(async_engine)
-
-
+session_factory = sessionmaker(sync_engine)
+async_session_factory = async_sessionmaker(async_engine)
 
 str_256 = Annotated[str, 256]
 
@@ -34,18 +29,14 @@ class Base(DeclarativeBase):
         str_256: String(256)
     }
 
+    repr_cols_num = 3
+    repr_cols = tuple()
+    
+    def __repr__(self):
+        """Relationships не используются в repr(), т.к. могут вести к неожиданным подгрузкам"""
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
 
-
-
-#with sync_engine.connect() as conn:
-#    res = conn.execute(text("SELECT VERSION()"))
-#    print(f"{res.first()=}")
-
-
-#async def get_123():
-#    async with async_engine.connect() as conn:
-#        res = await conn.execute(text("SELECT VERSION()"))
-#        print(f"{res.first()=}")
-
-
-#asyncio.run(get_123())
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
